b12835c43e29ba3e0683ffccf3a7ead8ff7e58dc
Arslan Ahmad <bitf16a018@pucit.edu.pk>
2020-02-07 17:50:03 +0500

    added a real calculator's UI

--- js.js
+++ js.js
@@ -1,15 +1,45 @@
 function func1(p) {
-	var a = document.getElementById("screen").value;
-	a = a + p;
-	document.getElementById("screen").value = a;
+    var a = document.getElementById("screen").value;
+    a = a + p;
+    document.getElementById("screen").value = a;
+}
+
+function divsign() {
+    var a = document.getElementById("screen").value;
+    a = a + "/";
+    document.getElementById("screen").value = a;
+}
+
+function mulsign() {
+    var a = document.getElementById("screen").value;
+    a = a + "*";
+    document.getElementById("screen").value = a;
+}
+
+function addsign() {
+    var a = document.getElementById("screen").value;
+    a = a + "+";
+    document.getElementById("screen").value = a;
+}
+
+function subsign() {
+    var a = document.getElementById("screen").value;
+    a = a + "-";
+    document.getElementById("screen").value = a;
+}
+
+function pntsign() {
+    var a = document.getElementById("screen").value;
+    a = a + ".";
+    document.getElementById("screen").value = a;
 }
 
 function func2() {
-	var a = document.getElementById("screen").value;
-	document.getElementById("result").value = eval(a);	
+    var a = document.getElementById("screen").value;
+    document.getElementById("result").value = eval(a);
 }
 
 function func3() {
-	var a = document.getElementById("screen").value;
-		document.getElementById("screen").value = null;
+    var a = document.getElementById("screen").value;
+    document.getElementById("screen").value = null;
 }
